e0f3f394ff598df49126cfe932c02103b089ed69
kc1r74p <7422353+kc1r74p@users.noreply.github.com>
2020-05-17 23:24:46 +0200

    Update startapp.sh

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -4,4 +4,4 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
 export HOME=/config
-exec /usr/bin/chromium-browser google.com --start-maximized --profile-directory=Default --use-gl=swiftshader --disable-software-rasterizer --disable-dev-shm-usage
+exec /usr/bin/chromium-browser $url --start-maximized --profile-directory=Default --use-gl=swiftshader --disable-software-rasterizer --disable-dev-shm-usage
